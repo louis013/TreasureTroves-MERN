4f3f2a585fef5714bf0ef4444296f15a7d9daa8b
angelo.louis13 <angelo.louis13@gmail.com>
2024-04-29 12:01:37 +0800

    Modified the cart page, display product name instead of product id

--- src/components/UserCart.js
+++ src/components/UserCart.js
@@ -1,20 +1,38 @@
-import {Card, Row, Col} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
 
-export default function UserCart({cart, cartItems}) {
+export default function UserCart({ cart, cartItems }) {
+    const [productDetails, setProductDetails] = useState([]);
 
-    if (!cart || cart.length === 0) {
+    useEffect(() => {
+        if (!cartItems || cartItems.length === 0) return;
+
+        cartItems.forEach((item, index) => {
+            fetch(`${process.env.REACT_APP_API_URL}/products/${item.productId}`)
+                .then(res => res.json())
+                .then(data => {
+                    // console.log(data);
+                    setProductDetails(prevState => [...prevState, data.product]);
+                });
+        });
+    }, [cartItems]);
+
+    if (!cart || !cartItems || cartItems.length === 0) {
         return <p>No items in cart</p>;
     }
 
+    // Filter out undefined or null values from cartItems
+    const validCartItems = cartItems.filter(item => item);
+
     return (
-        <Row className="mt-3 mb-3">
-            {cartItems.map((item, index) => (
+        <Row className="mt-3 mb-3 d-block">
+            {validCartItems.map((item, index) => (
                 <Col key={index}>
                     <Card id={`ProductComponent${index + 1}`}>
                         <Card.Body>
-                            <Card.Title className='mb-3'>{item.productId}</Card.Title>
-                            <Card.Subtitle className='mb-3'>Quantity: {item.quantity}</Card.Subtitle>
+                            <Card.Title>{productDetails[index]?.name}</Card.Title>
+                            <Card.Subtitle>Quantity</Card.Subtitle>
+                            <Card.Text>{item.quantity}</Card.Text>
                             <Card.Subtitle>Subtotal:</Card.Subtitle>
                             <Card.Text>Php {item.subtotal}</Card.Text>
                         </Card.Body>
@@ -22,6 +40,5 @@ export default function UserCart({cart, cartItems}) {
                 </Col>
             ))}
         </Row>
-    )
-
+    );
 }
